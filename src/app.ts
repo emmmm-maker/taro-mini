b64f5484cdf9ecf3ecc6b30eb98be813b070e22f
GG-Boy <xingchaochao@sina.com>
2023-11-07 13:09:57 +0800

    feat: 引入nutUI组件库

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import './app.scss';
+// 引入所有组件的样式文件
+import '@nutui/nutui-taro/dist/style.css';
 
 const App = createApp({
   onShow(options) {
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,7 @@
 <template>
   <view class="index">
     <text>{{ msg }}</text>
+    <nut-button type="primary">按钮测试</nut-button>
   </view>
 </template>
 
